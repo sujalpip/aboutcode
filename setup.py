ab07330584e87cad6838177c3ef7239488a117bb
Your Name <your_email@example.com>
2025-01-14 11:54:23 +0530

    sss

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name='AboutCode',
+    version='0.1.0',
+    description='A family of FOSS projects to uncover data about software',
+    author='Your Name',
+    author_email='your.email@example.com',
+    license='Apache-2.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
